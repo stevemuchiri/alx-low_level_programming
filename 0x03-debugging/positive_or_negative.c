9edc9e126da22430752c3bc08b4f112ce6bf981f
stevemuchiri <muchirindungu99@gmail.com>
2023-09-15 06:27:45 -0700

    debugg

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * positive_or_negative - Print random number states whether it
+ * @i : return number
+ *
+ * Return: Always 0.
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
+
+}
