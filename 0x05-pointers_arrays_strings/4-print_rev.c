4f10a7ed2dc29fa93688de652580b1081d0ac041
stevemuchiri <muchirindungu99@gmail.com>
2023-09-18 07:29:23 -0700

    resolved

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,12 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int i;
+	int count = 0;
 
-	while (s[index++])
-		len++;
-
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
-
+	for (i = 0; s[i] != '\0' ; i++)
+		count++;
+	for (i = count - 1; 1 >= 0 ; i--)
+		_putchar(s[i]);
 	_putchar('\n');
 }
