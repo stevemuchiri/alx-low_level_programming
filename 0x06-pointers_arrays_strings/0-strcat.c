67001d077f54610bee89eda62c86b36d02c02c3c
stevemuchiri <muchirindungu99@gmail.com>
2023-09-21 04:29:52 -0700

    correted

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,20 +12,20 @@ char *_strcat(char *dest, char *src)
 {
 	int k, r;
 
-k = 0;
-r = 0;
+	k = 0;
+	r = 0;
 
-while (dest[k] != '\0')
-{
-k++;
-}
+	while (dest[k] != '\0')
+	{
+		k++;
+	}
 
-while (src[r] != '\0')
-{
-dest[k] = src[r];
-k++;
-r++;
-}
-dest[k] = '\0';
-return (dest);
+	while (src[r] != '\0')
+	{
+		dest[k] = src[r];
+		k++;
+		r++;
+	}
+		dest[k] = '\0';
+	return (dest);
 }
