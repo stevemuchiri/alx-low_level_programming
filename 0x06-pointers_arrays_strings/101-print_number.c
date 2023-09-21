d79c913f724e5dfa03d2b58543c5eb0c869fe95f
stevemuchiri <muchirindungu99@gmail.com>
2023-09-21 11:06:07 -0700

    function that prints an integer.

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * print_number - main function
- *
- * @n: Function parameter
+ *print_number - main function.
+ * @n: Function parameter.
  *
  * Return: Always 0.
- */
+ */
 
 void print_number(int n)
 {
