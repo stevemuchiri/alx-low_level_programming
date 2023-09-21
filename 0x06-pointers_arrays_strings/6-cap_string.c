155cf59f8089c64ee593eb711b538359a99a16f7
stevemuchiri <muchirindungu99@gmail.com>
2023-09-21 10:36:49 -0700

    function that capitalizes all words of a string. function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,36 +1,37 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
-  * cap_string - The main  function
-  *
-  * @str: Function parameter of the string
-  *
-  * Return: Void.
-  */
-char *cap_string(char *str)
+ * cap_string - capitalize most of the words in a string.
+ * @s: analized string.
+ *
+ * Return: String with all words capitalized.
+ */
+char *cap_string(char *s)
 {
-int count = 0;
+	int i, j;
+	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-while (str[count])
-{
-while (!(str[count] >= 'a' && str[count] <= 'z'))
-count++;
-if (str[count - 1] == ' ' ||
-str[count - 1] == '\t' ||
-str[count - 1] == '\n' ||
-str[count - 1] == ',' ||
-str[count - 1] == ';' ||
-str[count - 1] == '.' ||
-str[count - 1] == '!' ||
-str[count - 1] == '?' ||
-str[count - 1] == '"' ||
-str[count - 1] == '(' ||
-str[count - 1] == ')' ||
-str[count - 1] == '{' ||
-str[count - 1] == '}' ||
-count == 0)
-str[count] -= 32;
-count++;
-}
-return (str);
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			if (i == 0)
+			{
+				*(s + i) = *(s + i) - 32;
+			}
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+					{
+						*(s + i) = *(s + i) - 32;
+					}
+				}
+			}
+		}
+	i++;
+	}
+	return (s);
 }
