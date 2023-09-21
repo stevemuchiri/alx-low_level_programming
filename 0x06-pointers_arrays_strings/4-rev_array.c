c77b4eff008e69550172601b512fa27fd27fe4b2
stevemuchiri <muchirindungu99@gmail.com>
2023-09-21 10:26:45 -0700

    function that reverses the content of an array of integers.

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,25 +1,22 @@
 #include "main.h"
 
 /**
-  * _strcmp - The main function
-  *
-  * @s1: Function parameter
-  *
-  * @s2: Function parameter
-  *
-  * Return: s1, s2
-  */
-int _strcmp(char *s1, char *s2)
+  * reverse_array - The main faunction
+  *
+  * @a: Function parameter
+  *
+  * @n: Function parameter
+  *
+  * Return: Always 0
+  */
+void reverse_array(int *a, int n)
 {
-int k = 0;
+	int k, r;
 
-while (s1[k] != '\0' && s2[k] != '\0')
-{
-if (s1[k] != s2[k])
-{
-return (s1[k] - s2[k]);
-}
-k++;
-}
-return (0);
+	for (k = 0; k < n--; k++)
+	{
+	r = a[k];
+	a[k] = a[n];
+	a[n] = r;
+	}
 }
