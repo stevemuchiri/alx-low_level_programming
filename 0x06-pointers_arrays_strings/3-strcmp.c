6e21a6843dda7729e476a5bd5d743321b22ec917
stevemuchiri <muchirindungu99@gmail.com>
2023-09-21 10:22:28 -0700

    a function that compares two strings.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,29 +1,25 @@
 #include "main.h"
 
 /**
-  * _strncpy - The main function of the code
-  *
-  * @src: Function parameter
-  *
-  * @dest: Function parameter
-  *
-  * @n: Function parameter
-  *
-  * Return: dest
-  */
-char *_strncpy(char *dest, char *src, int n)
+  * _strcmp - The main function
+  *
+  * @s1: Function parameter
+  *
+  * @s2: Function parameter
+  *
+  * Return: s1, s2
+  */
+int _strcmp(char *s1, char *s2)
 {
-int k = 0;
+int i = 0;
 
-while (k < n && src[k] != '\0')
+while (s1[i] != '\0' && s2[i] != '\0')
 {
-dest[k] = src[k];
-k++;
-}
-while  (k < n)
-{
-dest[k] = '\0';
-k++;
-}
-return (dest);
+	if (s1[i] != s2[i])
+	{
+	return (s1[i] - s2[i]);
+	}
+	i++;
+	}
+	return (0);
 }
