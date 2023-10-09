1944a828a01ad38b2a143a95558c9554ecdcc841
stevemuchiri <muchirindungu99@gmail.com>
2023-10-09 17:04:51 +0300

    hey

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,11 +2,11 @@
 #define FUNCTION_LIKE_MACRO_H
 
 /*
- * File: 3-function_like_macro.h
+ * File: function_like_macro.h
  * Desc: Function-like macro ABS(x)
  *       that computes the absolute value of a number x.
  */
 
-#define ABS(x) ((x) < 0 ? (-x) : (x))
+#define ABS(x) ((x) < (0) ? -(x) : (x))
 
 #endif
