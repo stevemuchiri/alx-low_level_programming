b735a8dac9c5281313acd74d2b9e9b77fdd68249
stevemuchiri <muchirindungu99@gmail.com>
2023-10-13 00:07:35 +0300

    done

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,11 +1,6 @@
 #ifndef VARIADIC_FUNTIONS_H
 #define VARIADIC_FUNTIONS_H
 
-/**
- * File: variadic_funtions.h
- * Desc: This  file  contains all funtion  prototypes  used 
- * in 0x10-variadic_functions folder.
- */
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
