44dbcddee7460cd03d7b85eb7cea406842cda8d9
stevemuchiri <muchirindungu99@gmail.com>
2023-10-24 14:37:25 +0300

    function that prints all the elements of a listint_t list.

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * print_listint - prints all the elements of a listint_t list.
- * @h: A pointer to the  head of the list.
+ * @h: A pointer to the  head of the list-t list.
  *
  * Return: The number of  nodes in a  list_t list.
  */
