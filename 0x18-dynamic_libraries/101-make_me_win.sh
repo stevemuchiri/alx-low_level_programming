df4616f6851318a93310c8c6908f20eb7d213851
stevemuchiri <muchirindungu99@gmail.com>
2024-02-02 23:50:56 +0300

    script

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/stevemuchiri/alx-low_level_programming/master/0x18-dynamic_libraries/libheck.so
+wget -p .. https://raw.githubusercontent.com/stevemuchiri/alx-low_level_programming/master/0x18-dynamic_libraries/libheck.so
 export LD_PRELOAD="$PWD/../libheck.so"
